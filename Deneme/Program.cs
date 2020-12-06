d84616cd53836803d6c6f5541ce4f3d4b5a2f24e
salih <salihkoc3469@gmail.com>
2020-12-06 15:55:57 +0300

    Deneme

--- Deneme/Program.cs
+++ Deneme/Program.cs
@@ -7,7 +7,7 @@ namespace Deneme
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            
+            Console.WriteLine("Deneme");
         }
     }
 }
